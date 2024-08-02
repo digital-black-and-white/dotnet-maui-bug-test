32dfaca756884805bb9db6c6db141ad07f99c876
Kevin Sanders <kbsanders@hotmail.com>
2024-08-02 17:28:56 -0400

    Bug test.

--- BugTest/MainPage.xaml.cs
+++ BugTest/MainPage.xaml.cs
@@ -1,34 +1,11 @@
-﻿using System.Collections.ObjectModel;
-
+﻿
 namespace BugTest;
 
 public partial class MainPage : ContentPage
 {
-	ObservableCollection<string> _list1;
-	ObservableCollection<string> _list2;
-
 	public MainPage()
 	{
 		InitializeComponent();
-
-		this._list1 = [
-			"Item 1",
-			"Item 2",
-			"Item 3",
-			"Item 4",
-			"Item 5"
-		];
-
-		this._list2 = [
-			"Item 1",
-			"Item 2",
-			"Item 3",
-			"Item 4",
-			"Item 5"
-		];
-
-		this.cboList1.ItemsSource = this._list1;
-		this.cboList2.ItemsSource = this._list2;
 	}
 
 	private void txtLog_TextChanged(object? sender, EventArgs e)
@@ -39,6 +16,8 @@ public partial class MainPage : ContentPage
 	private void btnTest_Clicked(object? sender, EventArgs e)
 	{
 		this.txtLog.Text += "btnTest_Clicked()" + Environment.NewLine;
+		this.txtLog.Text += "numTest1 Value: " + this.numTest1.Value + Environment.NewLine;
+		this.txtLog.Text += "numTest2 Value: " + this.numTest2.Value + Environment.NewLine;
 	}
 
 	private void btnTest_Focused(object? sender, FocusEventArgs e)
@@ -51,44 +30,23 @@ public partial class MainPage : ContentPage
 		this.txtLog.Text += "btnTest_Unfocused()" + Environment.NewLine;
 	}
 
-	private void cboList1_Focused(object? sender, FocusEventArgs e)
+	private void numTest1_Focused(object? sender, FocusEventArgs e)
 	{
-		this.txtLog.Text += "cboList1_Focused()" + Environment.NewLine;
+		this.txtLog.Text += "numTest1_Focused()" + Environment.NewLine;
 	}
 
-	private void cboList1_Unfocused(object? sender, EventArgs e)
+	private void numTest1_Unfocused(object? sender, EventArgs e)
 	{
-		this.txtLog.Text += "cboList1_Unfocused()" + Environment.NewLine;
+		this.txtLog.Text += "numTest1_Unfocused()" + Environment.NewLine;
 	}
 
-	private void cboList1_DropDownClosing(object? sender, EventArgs e)
+	private void numTest2_Focused(object? sender, FocusEventArgs e)
 	{
-		this.txtLog.Text += "cboList1_DropdownClosing()" + Environment.NewLine;
+		this.txtLog.Text += "numTest2_Focused()" + Environment.NewLine;
 	}
 
-	private void cboList1_DropDownClosed(object? sender, EventArgs e)
+	private void numTest2_Unfocused(object? sender, EventArgs e)
 	{
-		this.txtLog.Text += "cboList1_DropdownClosed()" + Environment.NewLine;
-	}
-
-	private void cboList2_Focused(object? sender, FocusEventArgs e)
-	{
-		this.txtLog.Text += "cboList2_Focused()" + Environment.NewLine;
-	}
-
-	private void cboList2_Unfocused(object? sender, EventArgs e)
-	{
-		this.txtLog.Text += "cboList2_Unfocused()" + Environment.NewLine;
-	}
-
-	private void cboList2_DropDownClosing(object? sender, EventArgs e)
-	{
-		this.txtLog.Text += "cboList2_DropdownClosing()" + Environment.NewLine;
-	}
-
-	private void cboList2_DropDownClosed(object? sender, EventArgs e)
-	{
-		this.txtLog.Text += "cboList2_DropdownClosed()" + Environment.NewLine;
+		this.txtLog.Text += "numTest2_Unfocused()" + Environment.NewLine;
 	}
 }
-
